0685cce970a6f9bab475816e8c1c94c9991cecba
unknown <beto.mvmava@gmail.com>
2024-03-06 18:31:09 -0600

    cambio 1

---
+++ Untitled-1.py
@@ -0,0 +1,10 @@
+fecha=input("que fecha es hoy ")
+cumpleaños=input("cuando es tu cumpleaños ")
+print("ingresa la fecha de tu cumpleaños")
+print("fecha de hoy = fecha de cumpleaños")
+
+
+print( fecha== cumpleaños)
+
+
+ 
---
+++ Untitled-3.py
@@ -0,0 +1,10 @@
+codigopostal= int (input("ingresa codigopostal "))
+minimo = 50000
+maximo = 50030
+if minimo <= codigopostal <= maximo :
+    codigopostal= (input("ingresa codigopostal"))
+    if ("verifica codigopostal") :
+        
+
+
+
